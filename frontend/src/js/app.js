14349143179025a85742d4d8b94337458234cc6b
katarzyna-stepczynska <stepczynska.katarzyna@gmail.com>
2019-12-07 13:54:27 +0100

    Make app view with sections to continiue working with app

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,9 +1,9 @@
-import { login } from './shared/login';
-import { register } from './shared/register';
+import login from './shared/login';
+import register from './shared/register';
 
 const init = () => {
-  const loginBtn = document.querySelector('#loginBtn');
-  loginBtn.addEventListener('click', login);
+  login();
+  register();
 };
 
 export default init;
