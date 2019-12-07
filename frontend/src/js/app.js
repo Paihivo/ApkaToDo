79d467c8e5fe36bad1f55488ec9a9c6e12b03e4d
Paweł Skwirowski <pskwirowski@gmail.com>
2019-12-07 22:40:25 +0100

    Add prettier code formatter and integrate it with eslint tool

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,10 +1,10 @@
-import "../css/main.css";
-import { login } from "./shared/login.js";
-import { register } from "./shared/register.js";
+import '../css/main.css';
+import { login } from './shared/login';
+import { register } from './shared/register';
 
 const init = () => {
-  const loginBtn = document.querySelector("#loginBtn");
-  loginBtn.addEventListener("click", login);
+  const loginBtn = document.querySelector('#loginBtn');
+  loginBtn.addEventListener('click', login);
 };
 
 init();
