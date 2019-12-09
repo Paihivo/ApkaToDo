8b9970f1d14165b7782da3357098d66610fac2cb
Kate <stepczynska.katarzyna@gmail.com>
2019-12-09 21:37:26 +0100

    Add adding new tasks

--- frontend/src/js/shared/task.js
+++ frontend/src/js/shared/task.js
@@ -1,13 +1,14 @@
 import clearRegions from './clear';
 export default function taskView() {
-  const taskBtn = document.querySelector('#taskBtn');
-  const taskReg = document.querySelector('#tasks-region');
+    const taskBtn = document.querySelector('#taskBtn');
+    const taskReg = document.querySelector('#tasks-region');
+    const navbarTitle = document.querySelector('.title-region');
 
-  taskBtn.addEventListener('click', e => {
-    e.preventDefault();
-    clearRegions();
-
-    const taskView = `
+    taskBtn.addEventListener('click', e => {
+        e.preventDefault();
+        clearRegions();
+        navbarTitle.innerHTML = 'Tasks TO-DO';
+        const taskView = `
             <div class="container">
                 <div class="card card-body">
                     <h2 class="title">Items</h2>
@@ -37,9 +38,14 @@ export default function taskView() {
                           </button>
                         </li>
                     </ul>
+                    <form id="addForm" class="form-inline">
+                    <input type="text" class="form-control" id="item" value="Add New Task....">
+                    <input type="submit" class="btn btn-dark" value="+">
+                    </form>
                 </div>
             </div>
         `;
+
     const taskContent = document.createElement('div');
     taskContent.innerHTML = taskView;
     taskReg.appendChild(taskContent);
@@ -51,5 +57,22 @@ export default function taskView() {
         item.remove();
       })
     );
+
+    const form = document.getElementById('addForm');
+    const itemList = document.getElementById('items');
+
+    function addItem(e) {
+      e.preventDefault();
+      const newItem = document.getElementById('item').value;
+      let li = document.createElement('li');
+      li.className = 'list-group-item';
+      console.log(li);
+      li.appendChild(document.createTextNode(newItem));
+      itemList.appendChild(li);
+    }
+
+    if(form) {
+      form.addEventListener('submit', addItem);
+    }
   });
 }
