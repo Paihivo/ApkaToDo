8a2084a4cd39b0e9bc965dd3aba4397ce2e113ea
Kucicowy <kucicowy@mail.ru>
2019-12-12 12:11:22 +0100

    Removed edit-task

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,9 +1,14 @@
 import { editAccount } from './shared/account';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import taskView from './shared/task';
 =======
 import { editTask } from './shared/edit-task';
 >>>>>>> Initial task edit modal
+=======
+import { editTask } from './shared/task';
+import taskView from './shared/task';
+>>>>>>> Removed edit-task
 
 const init = () => {
   taskView();
@@ -12,11 +17,13 @@ const init = () => {
     e.preventDefault();
     editAccount();
   });
+  taskView();
+  /*
   const testButton = document.querySelector('#testBtn');
   testButton.addEventListener('click', e => {
     e.preventDefault();
     editTask();
-  });
+  });*/
 };
 
 export default init;
--- frontend/src/js/shared/task.js
+++ frontend/src/js/shared/task.js
@@ -14,27 +14,23 @@ export default function taskView() {
                     <ul id="items" class="list-group">
                         <li id="task1" class="list-group-item">
                           <p>Item 1</p>
-                          <button data-task-id="task1" data-btn="remove">
-                            <i class="fa fa-trash" aria-hidden="true"></i>
-                          </button>
+                          <button data-task-id="task1" class="button button__edit data-toggle="modal" data-target="#editModal1"><i class="fas fa-edit" aria-hidden="true"></i></button>
+                          <button data-task-id="task1" class="button button__remove"><i class="fa fa-trash" aria-hidden="true"></i></button>
                         </li>
                         <li id="task2" class="list-group-item">
                           <p>Item 2</p>
-                          <button data-task-id="task2" data-btn="remove">
-                            <i class="fa fa-trash" aria-hidden="true"></i>
-                          </button>
-                        </li>
+                          <button data-task-id="task2" class="button button__edit data-toggle="modal" data-target="#editModal2"><i class="fas fa-edit" aria-hidden="true"></i></button>
+                          <button data-task-id="task2" class="button button__remove"><i class="fa fa-trash" aria-hidden="true"></i></button>
+                          </li>
                         <li id="task3" class="list-group-item">
                           <p>Item 3</p>
-                          <button data-task-id="task3" data-btn="remove">
-                            <i class="fa fa-trash" aria-hidden="true"></i>
-                          </button>
-                        </li>
+                          <button data-task-id="task3" class="button button__edit data-toggle="modal" data-target="#editModal3"><i class="fas fa-edit" aria-hidden="true"></i></button>
+                          <button data-task-id="task3" class="button button__remove"><i class="fa fa-trash" aria-hidden="true"></i></button>
+                          </li>
                         <li id="task4" class="list-group-item">
                           <p>Item 4</p>
-                          <button data-task-id="task4" data-btn="remove">
-                            <i class="fa fa-trash" aria-hidden="true"></i>
-                          </button>
+                          <button data-task-id="task4" class="button button__edit data-toggle="modal" data-target="#editModal4"><i class="fas fa-edit" aria-hidden="true"></i></button>
+                          <button data-task-id="task4" class="button button__remove"><i class="fa fa-trash" aria-hidden="true"></i></button>
                         </li>
                     </ul>
                 </div>
@@ -51,5 +47,37 @@ export default function taskView() {
         item.remove();
       })
     );
+    const editTask = (id) => {
+      const template = `
+      <div class="modal fade" id="${id}" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+              <div class="modal-content">
+              <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Edit task window</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                  </button>
+              </div>
+              <div class="modal-body">
+                  <input class="form__input" type="text" name="edittext" id="editTaskTextForm">
+                  <label class="form__label">Change Task</label>
+                  <input class="form__input" type="date" name="deadline" id="editTaskDeadline">
+                  <label class="form__label">Change date</label>
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+              </div>
+          </div>
+      </div>
+      `
+      document.querySelector('.button__edit').innerHTML += template;
+  };
   });
-}
+  const buttonEdit = document.querySelectorAll('.button__edit');
+    buttonEdit.forEach(button =>
+      button.addEventListener('click', function() {
+        editTask(button.dataset.target);
+      }));
+};
