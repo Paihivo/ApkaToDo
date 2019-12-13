15e34b4a678dd8f3594b2bf17ad10e06e7bcfd2e
Karol Wolski <karolwolski04@gmail.com>
2019-12-13 20:24:26 +0100

    Add get endpoint for all users and single user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,17 @@ const { User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  const users = await User.find().sort();
+  res.send(users);
+});
+
+router.get("/:id", async (req, res) => {
+  const users = await User.find().sort();
+  const user = users.filter(user => user._id == req.params.id);
+  res.send(user);
+});
+
 router.post('/', async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
