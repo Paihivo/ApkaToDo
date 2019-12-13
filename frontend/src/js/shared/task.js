4967dba5f9f9d0988023c05ee5dd11262832ca0d
Katarzyna <stepczynska.katarzyna@gmail.com>
2019-12-13 18:06:06 +0100

    Fix conflict

--- frontend/src/js/shared/task.js
+++ frontend/src/js/shared/task.js
@@ -1,51 +1,53 @@
+/* eslint-disable no-shadow */
 import clearRegions from './clear';
-export default function taskView() {
-    const taskBtn = document.querySelector('#taskBtn');
-    const taskReg = document.querySelector('#tasks-region');
-    const navbarTitle = document.querySelector('.title-region');
 
-    taskBtn.addEventListener('click', e => {
-        e.preventDefault();
-        clearRegions();
-        navbarTitle.innerHTML = 'Tasks TO-DO';
-        const taskView = `
-            <div class="container">
-                <div class="card card-body">
-                    <h2 class="title">Items</h2>
-                    <ul id="items" class="list-group">
-                        <li id="task1" class="list-group-item">
-                          <p>Item 1</p>
-                          <button data-task-id="task1" data-btn="remove">
-                            <i class="fa fa-trash" aria-hidden="true"></i>
-                          </button>
-                        </li>
-                        <li id="task2" class="list-group-item">
-                          <p>Item 2</p>
-                          <button data-task-id="task2" data-btn="remove">
-                            <i class="fa fa-trash" aria-hidden="true"></i>
-                          </button>
-                        </li>
-                        <li id="task3" class="list-group-item">
-                          <p>Item 3</p>
-                          <button data-task-id="task3" data-btn="remove">
-                            <i class="fa fa-trash" aria-hidden="true"></i>
-                          </button>
-                        </li>
-                        <li id="task4" class="list-group-item">
-                          <p>Item 4</p>
-                          <button data-task-id="task4" data-btn="remove">
-                            <i class="fa fa-trash" aria-hidden="true"></i>
-                          </button>
-                        </li>
-                    </ul>
-                    <form id="addForm" class="form-inline">
-                    <input type="text" class="form-control" id="item" value="Add New Task....">
-                    <input type="date" class="form-control" id="item-data" name="deadline">
-                    <input type="submit" class="btn btn-dark" value="+">
-                    </form>
-                </div>
-            </div>
-        `;
+export default function taskView() {
+  const taskBtn = document.querySelector('#taskBtn');
+  const taskReg = document.querySelector('#tasks-region');
+  const navbarTitle = document.querySelector('.title-region');
+
+  taskBtn.addEventListener('click', e => {
+    e.preventDefault();
+    clearRegions();
+    navbarTitle.innerHTML = 'Tasks TO-DO';
+    const taskView = `
+      <div class="container">
+        <div class="card card-body">
+          <h2 class="title">Items</h2>
+          <ul id="items" class="list-group">
+            <li id="task1" class="list-group-item">
+              <p>Item 1</p>
+              <button data-task-id="task1" data-btn="remove">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+              </button>
+            </li>
+            <li id="task2" class="list-group-item">
+              <p>Item 2</p>
+              <button data-task-id="task2" data-btn="remove">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+              </button>
+            </li>
+            <li id="task3" class="list-group-item">
+              <p>Item 3</p>
+              <button data-task-id="task3" data-btn="remove">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+              </button>
+            </li>
+            <li id="task4" class="list-group-item">
+              <p>Item 4</p>
+              <button data-task-id="task4" data-btn="remove">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+              </button>
+            </li>
+          </ul>
+          <form id="addForm" class="form-inline">
+            <input type="text" class="form-control" id="item" value="Add New Task....">
+            <input type="date" class="form-control" id="item-data" name="deadline">
+            <input type="submit" class="btn btn-dark" value="+">
+          </form>
+        </div>
+      </div>
+    `;
 
     const taskContent = document.createElement('div');
     taskContent.innerHTML = taskView;
@@ -53,7 +55,7 @@ export default function taskView() {
 
     const buttonsRemove = document.querySelectorAll('[data-btn="remove"]');
     buttonsRemove.forEach(button =>
-      button.addEventListener('click', function() {
+      button.addEventListener('click', () => {
         const item = document.getElementById(button.dataset.taskId);
         item.remove();
       })
@@ -63,18 +65,18 @@ export default function taskView() {
     const itemList = document.getElementById('items');
     const dateList = document.getElementById('item-data');
 
-    function addItem(e) {
-      e.preventDefault();
+    function addItem(event) {
+      event.preventDefault();
       const newItem = document.getElementById('item').value;
       const newItemData = dateList.value;
-      let li = document.createElement('li');
+      const li = document.createElement('li');
       li.className = 'list-group-item';
       console.log(li);
       li.appendChild(document.createTextNode(`${newItem} Deadline: ${newItemData}`));
       itemList.appendChild(li);
     }
 
-    if(form) {
+    if (form) {
       form.addEventListener('submit', addItem);
     }
   });
